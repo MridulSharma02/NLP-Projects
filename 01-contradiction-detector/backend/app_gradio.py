2a9ec15cbf887d20d349cdd1894f6bfb2b6d6a8b
Mridul Sharma <sharmamridul2121@gmail.com>
2026-08-19 01:20:44 +0530

    gradio 4.44.1, remove launch call for HF SDK

--- 01-contradiction-detector/backend/app_gradio.py
+++ 01-contradiction-detector/backend/app_gradio.py
@@ -36,5 +36,3 @@ demo = gr.Interface(
     description="Determine if two sentences are Consistent, Contradictory, or Unrelated"
 )
 
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
